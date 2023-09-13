3f6c474ee0e7adebd1014ba490e55bb2b1ccabaa
Gabriel Vicentin Negro <gabrielvicentinnegro@hotmail.com>
2023-09-13 10:24:32 -0300

    Criação do projeto no xCode

---
+++ PBF/PBF/PBFApp.swift
@@ -0,0 +1,17 @@
+//
+//  PBFApp.swift
+//  PBF
+//
+//  Created by Gabriel Vicentin Negro on 13/09/23.
+//
+
+import SwiftUI
+
+@main
+struct PBFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
